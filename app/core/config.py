f95f6632c9856c6008f0af16d49f36cbea6dc365
K Md Mamunur Rashid <vraxa.server@gmail.com>
2026-01-07 11:15:52 +0300

    Updated docker compose file

--- app/core/config.py
+++ app/core/config.py
@@ -8,26 +8,26 @@ load_dotenv()
 
 class Settings(BaseSettings):
     """Application settings and configuration."""
-    
+
     # Database
     DATABASE_URL: str
-    
+
     # JWT Settings
     SECRET_KEY: str = '67f79f54a85671dce02e3d8c3fc5e0b2'
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    
+
     # CORS
     ALLOWED_ORIGINS: str = "http://localhost:3000","http://127.0.0.1:3000","https://eygar.com"
-    
+
     # Service
-    SERVICE_NAME: str = "eygar-payment-service"
+    SERVICE_NAME: str = "eygarpayment"
 
     AUTH_SERVICE_URL: str = os.getenv('AUTH_SERVICE_URL', "http://127.0.0.1:8000/api/v1/auth")
     JWT_SECRET_KEY: str = "abcdefgh"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    
+
     class Config:
         env_file = ".env"
         case_sensitive = True
